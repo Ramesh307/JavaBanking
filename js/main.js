5e3b97da24d25112509323a7a4bafd263e64be60
unknown <basnetramesh487@gmail.com>
2023-06-04 14:44:55 +0545

    updated code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // Get Transaction Type:
 const transactType = document.querySelector("#transact-type");
 
@@ -6,9 +7,8 @@ const paymentCard = document.querySelector(".payment-card");
 const transferCard = document.querySelector(".transfer-card");
 const depositCard = document.querySelector(".deposit-card");
 const withdrawCard = document.querySelector(".withdraw-card");
-console.log(transactType)
+
 // Check For Transaction Type Evenet Listener:
-if(transactType){
 transactType.addEventListener("change",() =>{
 
     // Check For Transaction Type and Display Form:
@@ -16,19 +16,33 @@ transactType.addEventListener("change",() =>{
         case "payment":
             paymentCard.style.display = "block";
             paymentCard.nextElementSibling.style.display = "none";
-            depositCard.style.display = "none";
-            withdrawCard.style.display = "none";
+            
+           
         break;
 
         case "transfer":
             transferCard.previousElementSibling.style.display = "none";
             transferCard.style.display = "block";
-            transferCard.nextElementSibling.style.display = "none";
-            withdrawCard.style.display = "none";
+           
+            
         break;
-  
+
+        case "deposit":
+            paymentCard.style.display = "none";
+            depositCard.previousElementSibling.style.display = "none";
+            depositCard.style.display = "block";
+            depositCard.nextElementSibling.style.display = "none";
+        break;
+
+        case "withdraw":
+            withdrawCard.previousElementSibling.style.display = "none";
+            withdrawCard.style.display = "block";
+            transferCard.style.display = "none";
+            paymentCard.style.display = "none";
+        break;        
+        
     }
     // End Of Check For Transaction Type and Display Form.
 
-});}
+});
 // End Of Check For Transaction Type Evenet Listener.
